96931ee2dc935e9f4a57fcf0768f2e9c9c9de20f
brittany <bgrabowski@drew.edu>
2019-03-17 16:09:46 -0400

    Added Dropdown Menu

--- src/GradingView.jsx
+++ src/GradingView.jsx
@@ -99,6 +99,14 @@ class GradingView extends Component {
             <div>
                 <h1>{this.state.assignment_name}</h1>
                 
+                <select>
+                    <option value="needs_attention">Needs Attention</option>
+                    <option value="not_graded">Not Graded</option>
+                    <option value="graded">Graded</option>
+                </select>
+
+
+
                 {/* <button className="rubric-button-not-selected" onClick={(e) => this.handleSubmit(e)}>cat1</button> */}
                 {this.state.completedAPI ? this.studentLoop() : (<div>loading</div>)}
               
